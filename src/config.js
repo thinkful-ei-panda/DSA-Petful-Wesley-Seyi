f8c09c1c7d6fe4c66dbca0def0f54f98df3dd67f
SeyiAriyo <58642769+SeyiAriyo@users.noreply.github.com>
2020-09-19 16:03:55 -0400

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   API_ENDPOINT: 'https://secure-waters-44156.herokuapp.com/api/',
-  NODE_ENV: process.env.NODE_ENV || 'development', 
   API_KEY: 'b9%dp&:gV%DFxkVLK-.N(T/~#59<D;+mB,g/j&='
 
   //password 3fdc7d8c9b65bce91542f0f50d1d90ad850d97b7439012d32e3901e799b3f9e7
